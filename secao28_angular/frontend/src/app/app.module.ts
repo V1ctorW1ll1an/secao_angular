855be05ba100459137fc37c6546ff124a53a3515
Victor Willian de Sousa <victorwillian1012@gmail.com>
2020-10-06 02:26:12 -0300

    Create product form

--- secao28_angular/frontend/src/app/app.module.ts
+++ secao28_angular/frontend/src/app/app.module.ts
@@ -19,6 +19,9 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -43,6 +46,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatSnackBarModule,
     HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
