6c668e58a29227b8e5eb32f338647d1244b96269
Victor Willian de Sousa <victorwillian1012@gmail.com>
2020-10-05 23:28:54 -0300

    Create product component

--- secao28_angular/frontend/src/app/app.module.ts
+++ secao28_angular/frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { CustomForDirective } from './directives/custom-for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { CustomForDirective } from './directives/custom-for.directive';
     ProductCrudComponent,
     RedDirective,
     CustomForDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
