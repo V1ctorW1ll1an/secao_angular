0c9d4db2caa3cfa22005b34b8a1ad15676874126
Victor Willian de Sousa <victorwillian1012@gmail.com>
2020-10-07 01:58:52 -0300

    Delete product

--- secao28_angular/frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ secao28_angular/frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -4,11 +4,11 @@ import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.scss'],
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.scss'],
 })
-export class ProductUpdateComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
   product: Product;
 
   constructor(
@@ -19,16 +19,15 @@ export class ProductUpdateComponent implements OnInit {
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-
     this.productService.readById(id).subscribe((product) => {
       this.product = product;
       console.log(this.product);
     });
   }
 
-  updateProduct(): void {
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage('Produto atualizado!');
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluido com sucesso!');
       this.router.navigate(['/products']);
     });
   }
